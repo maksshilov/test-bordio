e848cbdb09e9fe09420bdd1376f73b0df2de8d7f
Max Shilov <dr.thomas.hewitt@gmail.com>
2023-10-15 22:18:32 +0300

    next

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
           </div>
           <div className="block-1">
             <div className="item-active">
-              <img src="/img/plaza/cinema.png" />
+              <img src="/img/plaza/cinema2.png" alt="" />
             </div>
             <div className="item-not-active">
               <img src="/img/plaza/logo-bowl.png" />
@@ -37,6 +37,59 @@ function App() {
             <div className="item-not-active">
               <img src="/img/plaza/logo-kids.png" />
             </div>
+          </div>
+          <div className="block-2">
+            <div className="item">
+              НОВЫЕ
+              <br />
+              КРЕСЛА
+            </div>
+            <div className="item">
+              НОВЫЙ
+              <br />
+              ЗВУК
+            </div>
+            <div className="item">
+              НОВЫЙ
+              <br />
+              СВЕТ
+            </div>
+            <div className="item">
+              НОВЫЙ
+              <br />
+              ЭКРАН
+            </div>
+          </div>
+          <div className="block-3">
+            <div className="tab">КИНО</div>
+            <div className="tab-contain">
+              <div className="description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla.
+              </div>
+              <div className="posters">
+                <div className="blur"></div>
+                <img src="/img/plaza/poster-0.png" />
+                <img src="/img/plaza/poster-1.png" />
+                <img src="/img/plaza/poster-2.png" />
+                <img src="/img/plaza/poster-3.png" />
+              </div>
+            </div>
+          </div>
+          <div className="footer">
+            <img src="/img/plaza/logo-white.png" alt="" className="logo" />
+            <div className="nav">
+              <div className="item">О НАС</div>
+              <div className="item">НОВОСТИ</div>
+              <div className="item">СЕАНСЫ</div>
+              <div className="item">КОНТАКТЫ</div>
+            </div>
+            <div className="tel">8 800 000 00 00</div>
+          </div>
+          <div className="footer-2">
+            <div className="item">©плаза 2023</div>
+            <div className="item">Политика конфиденциальности</div>
           </div>
         </AppContainer>
       </Provider>
--- src/AppContainer.ts
+++ src/AppContainer.ts
@@ -4,6 +4,7 @@ interface IProps {}
 
 export const AppContainer = styled.div<IProps>`
   display: flex;
+  align-items: center;
   flex-direction: column;
   width: 100vw;
   height: 100vh;
@@ -58,7 +59,6 @@ export const AppContainer = styled.div<IProps>`
       align-items: center;
       justify-content: space-between;
       width: 112px;
-      margin-left: 200px;
       & img {
         width: 50px;
       }
@@ -66,28 +66,154 @@ export const AppContainer = styled.div<IProps>`
   }
 
   & .block-1 {
+    margin-bottom: 5vh;
     display: flex;
     width: 100vw;
-    /* min-height: 750px; */
+    min-height: 600px;
+    overflow: hidden;
+    /* height: 100%; */
     & > .item-active {
       width: 60%;
-      /* height: 100%; */
+      height: 100%;
       & img {
+        object-fit: cover;
         width: 100%;
       }
     }
+
     & > .item-not-active {
       width: 20%;
-      height: 100%;
+      /* height: 100%; */
       display: flex;
       align-items: center;
       justify-content: center;
       background-color: #fbfbfb;
       border: 1px solid #d9d9d9;
       border-right: 0;
+      box-sizing: border-box;
       & img {
         height: 130px;
       }
+    }
+  }
+
+  & .block-2 {
+    margin-bottom: 5vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 60vw;
+    & > .item {
+      font-family: "NeueMachina-Bold";
+      font-size: 2vw;
+      color: #0046cc;
+      text-align: center;
+      line-height: 5vh;
+    }
+  }
+
+  & .block-3 {
+    margin-bottom: 5vh;
+    width: 75vw;
+
+    & .tab {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 25vw;
+      height: 4vw;
+      border: 2px solid #0046cc;
+      border-bottom: 0;
+      font-family: "NeueMachina-Bold";
+      font-size: 23px;
+      color: #0046cc;
+    }
+
+    & .tab-contain {
+      border: 2px solid #0046cc;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+      box-sizing: border-box;
+      padding: 2vw;
+
+      & .description {
+        width: 21vw;
+        font-family: "NeueMachina-Regular";
+        font-size: 16px;
+        line-height: 160%;
+      }
+
+      & .posters {
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        /* filter: drop-shadow(0 0 2vw #0046cc); */
+        /* box-shadow: 0 0 1vw #0046cc; */
+        & > img {
+          width: 10vw;
+          margin-right: 2vw;
+          &:last-of-type {
+            margin-right: 0;
+          }
+        }
+        & .blur {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          background-color: #0046cc66;
+          filter: blur(50px);
+          z-index: -1;
+        }
+      }
+    }
+  }
+
+  & .footer {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100vw;
+    min-height: 100px;
+    background-color: #0046cc;
+
+    & .logo {
+      width: 183px;
+    }
+
+    & .nav {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 501px;
+
+      & > .item {
+        font-family: "NeueMachina-Regular";
+        size: 18px;
+        color: white;
+      }
+    }
+    & .tel {
+      font-family: "NeueMachina-Bold";
+      font-size: 18px;
+      color: white;
+    }
+  }
+
+  & .footer-2 {
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #0046cc;
+    box-sizing: border-box;
+    padding: 26px 100px;
+    & > .item {
+      font-family: "NeueMachina-Regular";
+      font-size: 16px;
+      color: white;
     }
   }
 `;
