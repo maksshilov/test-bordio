15b2964ed663c9da06e7900630db56c3fb20d90e
Max Shilov <dr.thomas.hewitt@gmail.com>
2023-10-15 21:06:24 +0300

    next

--- src/App.tsx
+++ src/App.tsx
@@ -23,19 +23,19 @@ function App() {
               <div className="item">КОНТАКТЫ</div>
             </div>
             <div className="btns">
-              <img alt="" src="img/plaza/search.png" />
-              <img alt="" src="img/plaza/vk.png" />
+              <img alt="" src="/img/plaza/search.png" />
+              <img alt="" src="/img/plaza/vk.png" />
             </div>
           </div>
           <div className="block-1">
             <div className="item-active">
-              <img src="img/plaza/cinema.jpg" />
+              <img src="/img/plaza/cinema.jpg" />
             </div>
             <div className="item-not-active">
-              <img src="img/plaza/logo-bowl.png" />
+              <img src="/img/plaza/logo-bowl.png" />
             </div>
             <div className="item-not-active">
-              <img src="img/plaza/logo-kids.png" />
+              <img src="/img/plaza/logo-kids.png" />
             </div>
           </div>
         </AppContainer>
