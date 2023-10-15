4ef98369bb1a7ebc4ed358763c1f47f1fccf9a8c
Max Shilov <dr.thomas.hewitt@gmail.com>
2023-10-15 21:02:17 +0300

    next

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { AppContainer } from "./AppContainer";
-import { Content } from "./components/Content";
-import { SideBar } from "./components/SideBar";
-import { Tools } from "./components/Tools";
 import { store } from "./redux";
 import { Provider } from "react-redux";
-import Lottie from "lottie-react";
 
 function App() {
   const [intro, setIntro] = useState<boolean>(true);
@@ -18,17 +14,30 @@ function App() {
     <div>
       <Provider store={store}>
         <AppContainer>
-          {intro ? (
-            <div className="lottie">
-              <Lottie animationData={require("./lottie/bordio-intro.json")} autoPlay style={{ width: "50vw" }} />
+          <div className="header">
+            <img src="/img/plaza/logo.png" alt="" className="logo" />
+            <div className="nav">
+              <div className="item">О НАС</div>
+              <div className="item">НОВОСТИ</div>
+              <div className="item">СЕАНСЫ</div>
+              <div className="item">КОНТАКТЫ</div>
             </div>
-          ) : (
-            <>
-              <SideBar />
-              <Tools />
-              <Content />
-            </>
-          )}
+            <div className="btns">
+              <img alt="" src="img/plaza/search.png" />
+              <img alt="" src="img/plaza/vk.png" />
+            </div>
+          </div>
+          <div className="block-1">
+            <div className="item-active">
+              <img src="img/plaza/cinema.jpg" />
+            </div>
+            <div className="item-not-active">
+              <img src="img/plaza/logo-bowl.png" />
+            </div>
+            <div className="item-not-active">
+              <img src="img/plaza/logo-kids.png" />
+            </div>
+          </div>
         </AppContainer>
       </Provider>
     </div>
--- src/AppContainer.ts
+++ src/AppContainer.ts
@@ -4,6 +4,7 @@ interface IProps {}
 
 export const AppContainer = styled.div<IProps>`
   display: flex;
+  flex-direction: column;
   width: 100vw;
   height: 100vh;
 
@@ -29,5 +30,64 @@ export const AppContainer = styled.div<IProps>`
   }
 
   & .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100vw;
+    min-height: 100px;
+    background-color: white;
+
+    & .logo {
+      width: 183px;
+    }
+
+    & .nav {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 501px;
+
+      & > .item {
+        font-family: "NeueMachina-Regular";
+        size: 18px;
+        color: #0046cc;
+      }
+    }
+    & .btns {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 112px;
+      margin-left: 200px;
+      & img {
+        width: 50px;
+      }
+    }
+  }
+
+  & .block-1 {
+    display: flex;
+    width: 100vw;
+    /* min-height: 750px; */
+    & > .item-active {
+      width: 60%;
+      /* height: 100%; */
+      & img {
+        width: 100%;
+      }
+    }
+    & > .item-not-active {
+      width: 20%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #fbfbfb;
+      border: 1px solid #d9d9d9;
+      border-right: 0;
+      & img {
+        height: 130px;
+      }
+    }
   }
 `;
